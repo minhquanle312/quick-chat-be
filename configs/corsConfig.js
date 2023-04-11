af42dcd9c0334ae7373387fc54177ef7f066d286
Minh Quân Lê <minhquanle@gmail.com>
2023-04-11 17:03:43 +0700

    fix: update cors origin for fe website

--- configs/corsConfig.js
+++ configs/corsConfig.js
@@ -1,1 +1,5 @@
-exports.ALLOW_ORIGIN = ['http://localhost:3000', 'http://127.0.0.1:3000']
+exports.ALLOW_ORIGIN = [
+  'http://localhost:3000',
+  'http://127.0.0.1:3000',
+  'https://quick-chat-fe.vercel.app',
+]
